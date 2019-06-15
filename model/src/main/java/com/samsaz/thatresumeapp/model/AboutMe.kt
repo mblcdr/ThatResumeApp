4657a5d26a8e6648f308f11bef3d1bdd1bf0962d
Hamid Moazzami <mblcdr@gmail.com>
2019-06-16 00:22:35 +0430

    Add the missing adapter annotations on Models

--- model/src/main/java/com/samsaz/thatresumeapp/model/AboutMe.kt
+++ model/src/main/java/com/samsaz/thatresumeapp/model/AboutMe.kt
@@ -1,5 +1,8 @@
 package com.samsaz.thatresumeapp.model
 
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
 data class AboutMe(
     val name: String, val occupation: String, val imageLink: String?,
     val aboutMe: String
--- model/src/main/java/com/samsaz/thatresumeapp/model/Skill.kt
+++ model/src/main/java/com/samsaz/thatresumeapp/model/Skill.kt
@@ -1,5 +1,8 @@
 package com.samsaz.thatresumeapp.model
 
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
 data class Skill(
     val name: String, val imageLink: String?, val description: String,
     val color: String?
