f0f4590452f528707fa1b91e8e168b0f0b213d66
Hamid Moazzami <mblcdr@gmail.com>
2019-06-20 19:57:40 +0430

    Add the missing @JsonClass to Social model

--- model/src/main/java/com/samsaz/thatresumeapp/model/Social.kt
+++ model/src/main/java/com/samsaz/thatresumeapp/model/Social.kt
@@ -1,3 +1,6 @@
 package com.samsaz.thatresumeapp.model
 
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
 data class Social(val name: String, val imageLink: String? = null, val link: String?)
